be1eec1098e21a9e26e43a36cdf38f882b22a12c
Jean Guyomarc'h <jeanguyomarch.github@gmail.com>
2013-08-25 11:58:37 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+function err()
+{
+   echo >&2
+   echo "*** [install:error$?] $@" >&2
+   echo >&2
+   exit 2
+}
+
+function usage()
+{
+   echo "Usage: ./install.sh [prefix]. The default prefix is /usr/local/bin"
+}
+
+check_eet="`which eet`"
+check_os="`uname`"
+prefix="$1"
+
+if [ x"$1" == x"--help" ] || [ x"$1" == x"-help" ] || [ x"$1" == x"-h" ]; then
+   usage
+   exit 0
+fi
+
+echo "Checking for eet... $check_eet"
+if [ $? -ne 0 ]; then
+   err "'eet' not found"
+fi
+
+echo "Checking for OS... $check_os"
+if [ "$check_os" != "Darwin" ]; then
+   err "Mac OS X 'Darwin' required"
+fi
+
+if [ $# -eq 0 ]; then
+   prefix="/usr/local/bin"
+fi
+
+echo "Installing at path [$prefix]"
+
+sudo install -m 0755 pass "$prefix"
+
+
